1a07ed210787262a3bcb9408f7df83491f66de19
zmbfe <zmbfeng@gmail.com>
2025-09-20 18:14:46 -0400

    two pages navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { FindRate } from './features/find-rate/find-rate';
+import { Rates } from './features/rates/rates';
+export const routes: Routes = [
+  { path: '', redirectTo: 'find', pathMatch: 'full' }, // <-- default landing
+  { path: 'find', component: FindRate },
+  { path: 'rates', component: Rates },
+  { path: '**', redirectTo: 'find' }
+];
--- src/app/features/find-rate/find-rate.ts
+++ src/app/features/find-rate/find-rate.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-find-rate',
   imports: [],
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './find-rate.scss'
 })
 export class FindRate {
-
+  constructor(private router: Router) {}
+  goToRates() {
+    this.router.navigateByUrl('/rates');
+  }
 }
